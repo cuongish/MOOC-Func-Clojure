1fad0b7671dcf3a238124416cc8206ba4d283b55
Yogesh Pandit <yogeshp08@gmail.com>
2014-03-23 00:18:17 -0700

    Added training-day

---
+++ src/func_clojure/training_day.clj
@@ -0,0 +1,14 @@
+(ns func-clojure.training-day)
+
+(def answer
+  42)
+
+(def hai "O HAI!")
+
+(defn square [x]
+  "Returns the square"
+  (* x x))
+
+(defn average [a b]
+  "Returns the average"
+  (/ (+ a b) 2))
